5553447c7b5fb7052bd3654e4cfb291ec7a39442
unknown <archanashanmuga97@gmail.com>
2023-04-11 22:57:02 +0530

    latest commit

--- src/test/java/stepDefinitions/DataStepDefinitions.java
+++ src/test/java/stepDefinitions/DataStepDefinitions.java
@@ -14,65 +14,83 @@ import com.opencsv.exceptions.CsvException;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import java.io.*;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
 
 
 public class DataStepDefinitions  {
 	List<String[]> rows1 , rows2;
 
-	@Given("read data from Input File1")
-	public void read_data_from_input_file1() throws IOException, CsvException {
-		String csvFile = "C:\\Users\\sabar\\eclipse-workspace\\techMahindraProject\\CSV_Files\\InputFile1.csv";
-	        String line = "";
-	        String csvDelimiter = ",";
 
-	        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-	            while ((line = br.readLine()) != null) {
-	                String[] data = line.split(csvDelimiter);
-	                System.out.println("data read is -------"+data.length);
-	                System.out.println("data read is ++++++++"+data.hashCode());
-	                System.out.println("String read is ******"+String[].class);
-	                 // process the data array here
-	            }
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
-	
+	@Given("read data from Input File and merge in Output file")
+	public void read_data_from_input_file_and_merge_in_output_file() {
 
-	@Then("read data from Input File2")
-	public void read_data_from_input_file2() throws IOException, CsvException {
-		String csvFile2 = "C:\\Users\\sabar\\eclipse-workspace\\techMahindraProject\\CSV_Files\\InputFile2.csv";
-		CSVReader csvReader2 = new CSVReader(new BufferedReader(new FileReader(csvFile2)));
-		rows2 = csvReader2.readAll();
-		csvReader2.close();
-	}
 
-	@Then("write data at Output File")
-	public void write_data_at_output_file() throws IOException {
-		String outputCsvFile = "C:\\Users\\sabar\\eclipse-workspace\\techMahindraProject\\CSV_Files\\output.csv";
-		CSVWriter csvWriter = new CSVWriter(new FileWriter(outputCsvFile));
+		try {
+			// load the first excel file
+			FileInputStream file1 = new FileInputStream(new File("C:\\Users\\sabar\\eclipse-workspace\\techMahindraProject\\CSV_Files\\InputFile1.csv"));
+			XSSFWorkbook workbook1 = new XSSFWorkbook(file1);
+			XSSFSheet sheet1 = workbook1.getSheetAt(0);
 
-		// Perform the multiplication on the data from the CSV files
-		List<String[]> combinedRows = new ArrayList<String[]>();
-		for (int i = 0; i < rows1.size(); i++) {
-			String[] row1 = rows1.get(i);
-			String[] row2 = rows2.get(i);
-			double result = 0;
-			if(i==2) {
-				// Extract the required columns and perform the multiplication
-				double value1 = Double.parseDouble(row1[0]);
-				double value2 = Double.parseDouble(row2[1]);
-				 result = value1 * value2;
+			// load the second excel file
+			FileInputStream file2 = new FileInputStream(new File("C:\\Users\\sabar\\eclipse-workspace\\techMahindraProject\\CSV_Files\\InputFile2.csv"));
+			XSSFWorkbook workbook2 = new XSSFWorkbook(file2);
+			XSSFSheet sheet2 = workbook2.getSheetAt(0);
+
+			// create the output workbook and sheet
+			XSSFWorkbook outputWorkbook = new XSSFWorkbook();
+			XSSFSheet outputSheet = outputWorkbook.createSheet("C:\\Users\\sabar\\eclipse-workspace\\techMahindraProject\\CSV_Files\\Output");
+
+			// write the headers to the output sheet
+			Row headerRow = outputSheet.createRow(0);
+			headerRow.createCell(0).setCellValue("ID");
+			headerRow.createCell(1).setCellValue("PositionID");
+			headerRow.createCell(2).setCellValue("ISIN");
+			headerRow.createCell(3).setCellValue("Quantity");
+			headerRow.createCell(4).setCellValue("Total Price");
+
+			// iterate through the rows in the second sheet
+			for (int i = 1; i <= sheet2.getLastRowNum(); i++) {
+				XSSFRow row2 = sheet2.getRow(i);
+				int primaryID = (int) row2.getCell(1).getNumericCellValue();
+				int quantity = (int) row2.getCell(2).getNumericCellValue();
+
+				// look for matching row in the first sheet
+				for (int j = 1; j <= sheet1.getLastRowNum(); j++) {
+					XSSFRow row1 = sheet1.getRow(j);
+					if ((int) row1.getCell(0).getNumericCellValue() == primaryID) {
+						String ISIN = row1.getCell(2).getStringCellValue();
+						double unitPrice = row1.getCell(3).getNumericCellValue();
+						double totalPrice = quantity * unitPrice;
+
+						// write the values to the output sheet
+						Row outputRow = outputSheet.createRow(outputSheet.getLastRowNum() + 1);
+						outputRow.createCell(0).setCellValue(primaryID);
+						outputRow.createCell(1).setCellValue(row2.getCell(0).getStringCellValue());
+						outputRow.createCell(2).setCellValue(ISIN);
+						outputRow.createCell(3).setCellValue(quantity);
+						outputRow.createCell(4).setCellValue(totalPrice);
+					}
+				}
 			}
-			// Combine the data from both CSV files
-			String[] combinedRow = new String[] { row1[0], row2[1], row1[1], row2[0], String.valueOf(result) };
-			combinedRows.add(combinedRow);
+
+			// write the output file
+			FileOutputStream outFile = new FileOutputStream(new File("C:\\Users\\sabar\\eclipse-workspace\\techMahindraProject\\CSV_Files\\output.xlsx"));
+			outputWorkbook.write(outFile);
+			outFile.close();
+
+			// close all workbooks and streams
+			workbook1.close();
+			workbook2.close();
+			outputWorkbook.close();
+			file1.close();
+			file2.close();
+
+			System.out.println("Output file generated successfully.");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
-		// Write the combined data to the output CSV file
-		csvWriter.writeAll(combinedRows);
-
-		// Close the CSV writer
-		csvWriter.close();
 	}
 }
